85c6ab76092322c1c8c222e53a2a727082305b31
János Murvai-Gaál <janos.murvaigaal@outlook.com>
2015-07-05 09:17:22 +0200

    Test structure added

---
+++ elasticsearchrawclient/src/test/java/com/silverforge/elasticsearchrawclient/ConnectorTest.java
@@ -0,0 +1,12 @@
+package com.silverforge.elasticsearchrawclient;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ConnectorTest {
+
+	@Test
+	public void getTest() {
+		assertTrue("", true);
+	}
+}
