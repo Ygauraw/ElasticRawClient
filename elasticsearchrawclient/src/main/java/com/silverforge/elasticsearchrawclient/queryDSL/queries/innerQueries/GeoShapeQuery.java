62eaff440c182e59a08ef3bbd277f0cf903f0272
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2015-11-06 10:17:17 +0100

    google maps added as dependency to elasticrawclient

--- elasticsearchrawclient/src/main/java/com/silverforge/elasticsearchrawclient/queryDSL/queries/innerQueries/GeoShapeQuery.java
+++ elasticsearchrawclient/src/main/java/com/silverforge/elasticsearchrawclient/queryDSL/queries/innerQueries/GeoShapeQuery.java
@@ -1,12 +1,24 @@
 package com.silverforge.elasticsearchrawclient.queryDSL.queries.innerQueries;
 
+import android.graphics.Point;
+
 import com.silverforge.elasticsearchrawclient.queryDSL.definition.Queryable;
+
+import java.util.HashMap;
 
 public class GeoShapeQuery
     implements Queryable {
 
     @Override
     public String getQueryString() {
+//
+//
+//
+//        GeoJsonPoint point = new Point(new LatLng(0, 0));
+//        HashMap<String, String> properties = new HashMap<String, String>();
+//        properties.put(“Ocean”, “South Atlantic”);
+//        GeoJsonFeature pointFeature = new GeoJsonFeature(point, “Origin”, properties, null);
+
         return null;
     }
 }
