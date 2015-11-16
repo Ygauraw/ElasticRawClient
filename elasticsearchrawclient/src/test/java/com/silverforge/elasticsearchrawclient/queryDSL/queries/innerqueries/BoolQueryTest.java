1f7a23adfbefd77b7cbf4433e629138e369171f7
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2015-11-16 14:33:07 +0100

    BoolQuery mocked

--- elasticsearchrawclient/src/test/java/com/silverforge/elasticsearchrawclient/queryDSL/queries/innerqueries/BoolQueryTest.java
+++ elasticsearchrawclient/src/test/java/com/silverforge/elasticsearchrawclient/queryDSL/queries/innerqueries/BoolQueryTest.java
@@ -3,6 +3,7 @@ package com.silverforge.elasticsearchrawclient.queryDSL.queries.innerQueries;
 import com.silverforge.elasticsearchrawclient.BuildConfig;
 import com.silverforge.elasticsearchrawclient.exceptions.MandatoryParametersAreMissingException;
 import com.silverforge.elasticsearchrawclient.queryDSL.definition.QueryTest;
+import com.silverforge.elasticsearchrawclient.queryDSL.definition.Queryable;
 import com.silverforge.elasticsearchrawclient.queryDSL.operators.LogicOperator;
 import com.silverforge.elasticsearchrawclient.queryDSL.operators.ZeroToOneRangeOperator;
 
@@ -17,6 +18,7 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
@@ -29,38 +31,30 @@ public class BoolQueryTest {
     public void when_query_fully_defined_then_query_generated_with_must_should_must_not()
             throws MandatoryParametersAreMissingException {
 
+        Queryable matchQueryable = mock(Queryable.class);
+        when(matchQueryable.getQueryString()).thenReturn("{\"match\":{\"name\":\"Karcag\"}}");
+
+        Queryable matchAllQueryable = mock(Queryable.class);
+        when(matchAllQueryable.getQueryString()).thenReturn("{\"match_all\":{}}");
+
+        Queryable multiMatchQueryable = mock(Queryable.class);
+        when(multiMatchQueryable.getQueryString()).thenReturn("{\"multi_match\":{\"fields\":[\"name\",\"population\"],\"query\":\"Karcag Budapest\",\"use_dis_max\":\"false\",\"analyzer\":\"standard\",\"tie_breaker\":\"0.4\"}}");
+
+        Queryable complicatedMatchQueryable = mock(Queryable.class);
+        when(complicatedMatchQueryable.getQueryString()).thenReturn("{\"match\":{\"name\":{\"query\":\"Karcag Budapest\",\"operator\":\"or\",\"analyzer\":\"keyword\"}}}");
+
         BoolQuery query = BoolQuery
             .builder()
             .must(
-                MatchQuery
-                    .builder()
-                    .allFields()
-                    .build(),
-                MatchQuery
-                    .builder()
-                    .fieldName("name")
-                    .value("Karcag")
-                    .build())
+                matchAllQueryable,
+                matchQueryable)
             .should(
-                MultiMatchQuery
-                    .builder()
-                    .fields("name", "population")
-                    .tieBreaker(ZeroToOneRangeOperator._0_4)
-                    .query("Karcag Budapest")
-                    .useDisMax(false)
-                    .analyzer("standard")
-                    .build())
+                multiMatchQueryable)
             .mustNot(
                 BoolQuery
                     .builder()
                     .should(
-                        MatchQuery
-                            .builder()
-                            .fieldName("name")
-                            .query("Karcag Budapest")
-                            .operator(LogicOperator.OR)
-                            .analyzer("keyword")
-                            .build())
+                        complicatedMatchQueryable)
                     .build())
             .minimumShouldMatch(23)
             .disableCoord(false)
@@ -82,6 +76,75 @@ public class BoolQueryTest {
         assertThat(queryString.indexOf("\"minimum_should_match\":\"23\""), greaterThan(0));
         assertThat(queryString.indexOf("\"disable_coord\":\"false\""), greaterThan(0));
         assertThat(queryString.indexOf("\"boost\":\"1.0\""), greaterThan(0));
+    }
+
+    @Test
+    public void when_must_defined_then_query_generated_with_must()
+            throws MandatoryParametersAreMissingException {
+
+        Queryable matchQueryable = mock(Queryable.class);
+        when(matchQueryable.getQueryString()).thenReturn("{\"match\":{\"name\":\"Karcag\"}}");
+
+        BoolQuery query = BoolQuery
+            .builder()
+            .must(matchQueryable)
+            .build();
+
+        String queryString = query.getQueryString();
+
+        assertThat(queryString, notNullValue());
+        assertThat(queryString, not(""));
+
+        assertThat(queryString.startsWith("{\"bool\":{"), is(true));
+        assertThat(queryString.endsWith("}}"), is(true));
+
+        assertThat(queryString.indexOf("\"must\":[{\"match\":{\"name\":\"Karcag\"}}]"), greaterThan(0));
+    }
+
+    @Test
+    public void when_must_not_defined_then_query_generated_with_must_not()
+            throws MandatoryParametersAreMissingException {
+
+        Queryable matchAllQueryable = mock(Queryable.class);
+        when(matchAllQueryable.getQueryString()).thenReturn("{\"match_all\":{}}");
+
+        BoolQuery query = BoolQuery
+            .builder()
+            .mustNot(matchAllQueryable)
+            .build();
+
+        String queryString = query.getQueryString();
+
+        assertThat(queryString, notNullValue());
+        assertThat(queryString, not(""));
+
+        assertThat(queryString.startsWith("{\"bool\":{"), is(true));
+        assertThat(queryString.endsWith("}}"), is(true));
+
+        assertThat(queryString.indexOf("\"must_not\":[{\"match_all\":{}}]"), greaterThan(0));
+    }
+
+    @Test
+    public void when_should_defined_then_query_generated_with_should()
+            throws MandatoryParametersAreMissingException {
+
+        Queryable matchAllQueryable = mock(Queryable.class);
+        when(matchAllQueryable.getQueryString()).thenReturn("{\"match_all\":{}}");
+
+        BoolQuery query = BoolQuery
+            .builder()
+            .should(matchAllQueryable)
+            .build();
+
+        String queryString = query.getQueryString();
+
+        assertThat(queryString, notNullValue());
+        assertThat(queryString, not(""));
+
+        assertThat(queryString.startsWith("{\"bool\":{"), is(true));
+        assertThat(queryString.endsWith("}}"), is(true));
+
+        assertThat(queryString.indexOf("\"should\":[{\"match_all\":{}}]"), greaterThan(0));
     }
 
     // endregion
